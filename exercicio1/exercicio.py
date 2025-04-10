61fa16c6ab432bc8deabbe64738bf924d243ea53
Geovanna Almeida <geovannaludmila7@gmail.com>
2025-04-10 14:11:47 +0000

    exercicioa

---
+++ exercicio1/exercicio.py
@@ -0,0 +1,3 @@
+nome = input ("digite seu nome:")
+matricula = input ( " digite sua matricula:")
+print(nome, matricula)
---
+++ exercicio1/exercicio2.py
@@ -0,0 +1,3 @@
+nome = input ("digite seu nome:")
+matricula = input ( " digite sua matricula:")
+print(f"bem vindo ao codespace {nome}")
