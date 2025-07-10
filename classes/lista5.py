afda3c7eddb61838a230921f918af879dddc43f2
Geovanna Almeida <geovannaludmila7@gmail.com>
2025-07-10 14:26:06 +0000

    POO

--- classes/lista5.py
+++ classes/lista5.py
@@ -5,25 +5,38 @@ class Paciente:
         self.__nome = nome
         self.__cpf = cpf
         self.__telefone = telefone
-        self.__nascimento = datetime.strptime(nascimento, "%d/%m/%Y").date()
+        self.__nascimento = nascimento
 
     def set_nome(self, nome):
         if nome == "": raise ValueError
         self.__nome = nome
+
     def set_cpf(self, cpf):
-        if cpf > 0 : raise ValueError
+        if not cpf or not isinstance(cpf, str):
+            raise ValueError("CPF inválido.")
+        self.__cpf = cpf
+
     def set_telefone(self, telefone):
-        if telefone > 0: raise ValueError
+        if not telefone or not isinstance(telefone, str):
+            raise ValueError("Telefone inválido.")
+        self.__telefone = telefone
+
     def set_nascimento(self, nascimento):
-        self.__nascimento = datetime.strptime(nascimento, "%d/%m/%Y").date()
+        try:
+            self.__nascimento = datetime.strptime(nascimento, "%d/%m/%Y").date() 
+        except ValueError:
+            raise ValueError("Data de nascimento inválida. Use o formato DD/MM/AAAA.")
 
 
     def get_nome(self):
         return self.__nome
+    
     def get_cpf(self): 
         return self.__cpf
+    
     def get_telefone(self): 
         return self.__telefone
+    
     def get_nascimento(self):
         return self.__nascimento
 
@@ -43,3 +56,6 @@ class Paciente:
         return f"Nome: {self.__nome}\nCPF: {self.__cpf}\nTelefone: {self.__telefone}\nNascimento: {self.__nascimento.strftime('%d/%m/%Y')}\nIdade: {self.idade()}"
     
 
+class UI():
+     def main: 
+        
